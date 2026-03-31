7164750965dbda0dcd087c65726e89628ac1261c
Virgil I Ambar <imanuelambar11@gmail.com>
2026-03-31 17:27:55 +0700

    feat: update user model

--- src/model/users.model.js
+++ src/model/users.model.js
@@ -9,28 +9,6 @@ import { pool } from "../lib/db.js";
 
 /**
  *
- * @type {User[]}
- */
-const usersData = [
-    { id: 1, email: "test@gmail.com", password: "12345" },
-    { id: 2, email: "piu@gmail.com", password: "54321" },
-    { id: 3, email: "asd@gmail.com", password: "asdas1" },
-    { id: 4, email: "iho@gmail.com", password: "nfds54dsg321" },
-    { id: 5, email: "yqufd@gmail.com", password: "acv54dnf321" },
-    { id: 6, email: "nklva@gmail.com", password: "543jt21" },
-    { id: 8, email: "oklnjh@gmail.com", password: "je54321" },
-    { id: 9, email: "sdbs@gmail.com", password: "mgf54321" },
-    { id: 10, email: "mkl@gmail.com", password: "yuo54321" },
-];
-
-// console.log(usersData.length)
-
-function findUserIndex(id) {
-    return usersData.findIndex((user) => user.id === id);
-}
-
-/**
- *
  * @returns {User[]}
  */
 export async function getAllUsers() {
@@ -85,61 +63,4 @@ export async function getUserByEmail(email) {
     return data[0]; 
 }
 
-let lastId = 10;
 
-/**
- *
- * @param {User} data
- * @returns {User}
- */
-export async function createUser(data) {
-    try {
-        const get = await getUserByEmail(data.email);
-        if (get.email === data.email) {
-            throw new Error("user already exists");
-        }
-    } catch {
-        const newUser = {
-            id: ++lastId,
-            ...data,
-        };
-
-        usersData.push(newUser);
-
-        return newUser;
-    }
-}
-
-/**
- *
- * @param {number} id
- * @param {User} data
- * @returns {User}
- */
-export async function updateUser(id, data) {
-    const index = findUserIndex(id);
-    if (index !== -1) {
-        usersData[index] = {
-            ...usersData[index],
-            ...data,
-        };
-        return usersData[index];
-    } else {
-        throw new Error("user not found");
-    }
-}
-
-/**
- * @param {number} id
- * @returns {User}
- */
-export async function deleteUser(id) {
-    const found = findUserIndex(id);
-    const user = usersData[found];
-    if (found !== -1) {
-        usersData.splice(found, 1);
-        return user;
-    } else {
-        throw new Error("user not found");
-    }
-}
