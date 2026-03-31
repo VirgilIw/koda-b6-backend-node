602f960f65e29602c41d5e067c27f2811a174a0a
Virgil I Ambar <imanuelambar11@gmail.com>
2026-03-31 17:07:00 +0700

    fix: move route and middleware from main.js

--- src/main.js
+++ src/main.js
@@ -1,26 +1,13 @@
-import express from "express";
-import { constants } from "node:http2";
+import app from "./app.js";
+import { db } from "./lib/db.js";
 
-import userRouter from "./router/users.router.js";
-import authRouter from "./router/auth.router.js";
+export const conn = db();
 
-const app = express();
+function main() {
+    const PORT = process.env.PORT || 8888;
+    app.listen(PORT, function () {
+        console.log(`app listening on port ${PORT}`);
+    });
+}
 
-app.use(express.json()); // global
-
-// group
-app.use("/auth",authRouter)
-app.use("/users",userRouter)
-
-app.get("/", function (req, res) {
-  res.status(constants.HTTP_STATUS_OK).json({
-    success: true,
-    message: "backend is running well",
-  });
-});
-
-const PORT = process.env.PORT
-
-app.listen(PORT, function () {
-  console.log(`app listening on port ${PORT}`);
-});
+main();
