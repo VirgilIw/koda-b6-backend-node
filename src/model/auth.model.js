c4e6cc8c2d490bbe17897998512d553e2e8d8fb0
Virgil I Ambar <imanuelambar11@gmail.com>
2026-03-31 17:05:24 +0700

    feat: add auth model and update user model

---
+++ src/model/auth.model.js
@@ -0,0 +1,29 @@
+/**
+ * @typedef {Object} User
+ * @property {number} id
+ * @property {string} email
+ * @property {string} password
+ */
+
+import { pool } from "../lib/db.js";
+
+/**
+ * 
+ * @param {string} data 
+ * @returns {User}
+ */
+export async function register(data) {
+    const sql = `
+        INSERT INTO users(email, password) 
+        VALUES($1, $2)
+        RETURNING id, email;
+    `;
+
+    const values = [data.email, data.password];
+
+    const {
+        rows: [rgstr],
+    } = await pool.query(sql, values);
+
+    return rgstr;
+}
--- src/model/users.model.js
+++ src/model/users.model.js
@@ -5,51 +5,41 @@
  * @property {string} password
  */
 
+import { pool } from "../lib/db.js";
+
 /**
  *
  * @type {User[]}
  */
 const usersData = [
-  { id: 1, email: "test@gmail.com", password: "12345" },
-  { id: 2, email: "piu@gmail.com", password: "54321" },
-  { id: 3, email: "asd@gmail.com", password: "asdas1" },
-  { id: 4, email: "iho@gmail.com", password: "nfds54dsg321" },
-  { id: 5, email: "yqufd@gmail.com", password: "acv54dnf321" },
-  { id: 6, email: "nklva@gmail.com", password: "543jt21" },
-  { id: 8, email: "oklnjh@gmail.com", password: "je54321" },
-  { id: 9, email: "sdbs@gmail.com", password: "mgf54321" },
-  { id: 10, email: "mkl@gmail.com", password: "yuo54321" },
+    { id: 1, email: "test@gmail.com", password: "12345" },
+    { id: 2, email: "piu@gmail.com", password: "54321" },
+    { id: 3, email: "asd@gmail.com", password: "asdas1" },
+    { id: 4, email: "iho@gmail.com", password: "nfds54dsg321" },
+    { id: 5, email: "yqufd@gmail.com", password: "acv54dnf321" },
+    { id: 6, email: "nklva@gmail.com", password: "543jt21" },
+    { id: 8, email: "oklnjh@gmail.com", password: "je54321" },
+    { id: 9, email: "sdbs@gmail.com", password: "mgf54321" },
+    { id: 10, email: "mkl@gmail.com", password: "yuo54321" },
 ];
 
 // console.log(usersData.length)
 
 function findUserIndex(id) {
-  return usersData.findIndex((user) => user.id === id);
+    return usersData.findIndex((user) => user.id === id);
 }
 
 /**
  *
  * @returns {User[]}
  */
-export function findAll() {
-  return usersData;
-}
+export async function getAllUsers() {
+    const sql = `
+ select id, email, password from users
+ order by id asc;`;
 
-export async function getAllUsers(page = 1, limit = 5) {
-  const users = findAll();
-
-  const total = users.length;
-  const start = (page - 1) * limit;
-  const end = start + limit;
-
-  const data = users.slice(start, end);
-
-  return {
-    data,
-    total,
-    page,
-    totalPages: Math.ceil(total / limit),
-  };
+    const { rows: user } = await pool.query(sql);
+    return user;
 }
 
 /**
@@ -57,29 +47,42 @@ export async function getAllUsers(page = 1, limit = 5) {
  * @returns {User}
  */
 export async function getUserById(id) {
-  const found = usersData.filter((user) => user.id == id);
-  if (found.length === 1) {
-    return found[0];
-  } else {
-    throw new Error("user not found");
-  }
+    const sql = `
+ select id, email, password from users
+ where id = $1
+ order by id asc;`;
+
+    const values = [id];
+
+    const {
+        rows: [user],
+    } = await pool.query(sql, values);
+    if (!user) {
+        throw new Error("user not found");
+    }
+    return user;
 }
 
 /**
  * @param {string} email
  * @returns {User}
  */
-export async function getUserByEmail(email, cb) {
-  const found = usersData.filter((user) => user.email == email);
-  if (found.length === 1) {
-    return found[0];
-  } else {
-    if (cb) {
-      cb("user not found");
-    } else {
-      throw new Error("user not found");
+export async function getUserByEmail(email) {
+    const sql = `
+    SELECT id, email, password 
+    FROM users 
+    WHERE email = $1
+  `;
+
+    const values = [email];
+
+    const { rows: data } = await pool.query(sql, values);
+
+    if (data.length === 0) {
+        throw new Error("user not found");
     }
-  }
+
+    return data[0]; 
 }
 
 let lastId = 10;
@@ -90,21 +93,21 @@ let lastId = 10;
  * @returns {User}
  */
 export async function createUser(data) {
-  try {
-    const get = await getUserByEmail(data.email);
-    if (get.email === data.email) {
-      throw new Error("user already exists");
+    try {
+        const get = await getUserByEmail(data.email);
+        if (get.email === data.email) {
+            throw new Error("user already exists");
+        }
+    } catch {
+        const newUser = {
+            id: ++lastId,
+            ...data,
+        };
+
+        usersData.push(newUser);
+
+        return newUser;
     }
-  } catch {
-    const newUser = {
-      id: ++lastId,
-      ...data,
-    };
-
-    usersData.push(newUser);
-
-    return newUser;
-  }
 }
 
 /**
@@ -114,16 +117,16 @@ export async function createUser(data) {
  * @returns {User}
  */
 export async function updateUser(id, data) {
-  const index = findUserIndex(id);
-  if (index !== -1) {
-    usersData[index] = {
-      ...usersData[index],
-      ...data,
-    };
-    return usersData[index];
-  } else {
-    throw new Error("user not found");
-  }
+    const index = findUserIndex(id);
+    if (index !== -1) {
+        usersData[index] = {
+            ...usersData[index],
+            ...data,
+        };
+        return usersData[index];
+    } else {
+        throw new Error("user not found");
+    }
 }
 
 /**
@@ -131,12 +134,12 @@ export async function updateUser(id, data) {
  * @returns {User}
  */
 export async function deleteUser(id) {
-  const found = findUserIndex(id);
-  const user = usersData[found];
-  if (found !== -1) {
-    usersData.splice(found, 1);
-    return user;
-  } else {
-    throw new Error("user not found");
-  }
+    const found = findUserIndex(id);
+    const user = usersData[found];
+    if (found !== -1) {
+        usersData.splice(found, 1);
+        return user;
+    } else {
+        throw new Error("user not found");
+    }
 }
