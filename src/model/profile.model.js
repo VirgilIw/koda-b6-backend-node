57581c51cc5a704df8b08fa3e768c0b6709ab3ea
Virgil I Ambar <imanuelambar11@gmail.com>
2026-04-03 12:11:53 +0700

    feat: add new model for profile then add two function for get profile andd update profile

---
+++ src/model/profile.model.js
@@ -0,0 +1,69 @@
+import { pool } from "../lib/db.js";
+
+export async function getProfile() {
+    const query = `
+    SELECT 
+        id,
+        fullname,
+        email,
+        password,
+        phone,
+        address,
+        picture,
+        role,
+        created_at,
+        updated_at,
+        deleted_at,
+        lastlogin_at
+    FROM users;`;
+
+    const { rows: result } = await pool.query(query);
+    return result[0];
+}
+
+/**
+ * @param {Object} user
+ * @param {number} user.id
+ * @param {string} user.fullname
+ * @param {string} user.email
+ * @param {string} user.password
+ * @param {string} user.phone
+ * @param {string} user.address
+ * @param {string} user.picture
+ * @param {string} user.role
+ * @returns {Promise<User>}
+ */
+/**
+ * @param {number} id
+ * @param {Object} data
+ * @returns {Promise<User>}
+ */
+export async function updateProfile(id, data) {
+    const sql = `
+UPDATE users SET
+    fullname = $1,
+    email = $2,
+    password = $3,
+    phone = $4,
+    address = $5,
+    picture = $6,
+    role = $7,
+    updated_at = now()
+WHERE id = $8
+RETURNING id, fullname, email, phone, address, picture, role;
+    `;
+
+    const values = [
+        data.fullname,
+        data.email,
+        data.password,
+        data.phone,
+        data.address,
+        data.picture,
+        data.role,
+        id,
+    ];
+
+    const { rows } = await pool.query(sql, values);
+    return rows[0];
+}
