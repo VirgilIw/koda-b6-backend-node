c7bc0b93337bfe8179fd27e8b4a5dbcc65715f8a
Virgil I Ambar <imanuelambar11@gmail.com>
2026-04-03 12:51:57 +0700

    feat: add cors for resource sharing

--- src/app.js
+++ src/app.js
@@ -2,8 +2,19 @@ import express from "express";
 import adminRouter from "./router/admin.router.js";
 import authRouter from "./router/auth.router.js";
 import mainRouter from "./router/main.router.js";
+import docsRouter from "./router/docs.js";
+import profileRouter from "./router/profile.router.js";
+import cors from "cors";
 
 const app = express();
+
+app.use(
+    cors({
+        origin: ["http://localhost:5173"],
+        methods: ["GET", "PATCH", "POST", "DELETE"],
+        allowedHeaders: ["Content-Type", "Authorization"],
+    }),
+);
 
 // middleware
 app.use(express.json());
@@ -11,8 +22,10 @@ app.use(express.json());
 // routes
 app.use("/main", mainRouter);
 app.use("/auth", authRouter);
-// app.use("/admin", auth, adminRouter);
 app.use("/admin", adminRouter);
+app.use("/profile", profileRouter);
 
+// swagger
+app.use("/swagger", docsRouter);
 
 export default app;
