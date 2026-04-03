2526913ccf125db37fa49ec0c4499c09c972ad06
Virgil I Ambar <imanuelambar11@gmail.com>
2026-04-03 11:40:46 +0700

    feat: add new middleware for upload file image

---
+++ src/middleware/upload.middleware.js
@@ -0,0 +1,30 @@
+import multer from "multer";
+import { nanoid } from "nanoid";
+import path from "path";
+
+const storage = (folder) =>
+    multer.diskStorage({
+        destination: function (req, file, cb) {
+            // err
+            // path
+            // nanti bisa di-upgrade pakai path.join
+            cb(null, folder); 
+        },
+        filename: function (req, file, cb) {
+            const randStr = nanoid();
+            const ext = path.extname(file.originalname);
+            const newFileName = `${randStr}${ext}`;
+            cb(null, newFileName);
+        },
+    });
+
+function uploadMiddleware(folder) {
+    return multer({
+        storage: storage(folder),
+        limits: {
+            fileSize: 10 * 1024 * 1024,
+        },
+    });
+}
+
+export default uploadMiddleware;
