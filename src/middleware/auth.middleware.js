458c43667bc5d7e6e989f6ddebf4ee115221c0a6
Virgil I Ambar <imanuelambar11@gmail.com>
2026-04-03 12:09:21 +0700

    fix: fix auth middleware

--- src/middleware/auth.middleware.js
+++ src/middleware/auth.middleware.js
@@ -9,19 +9,31 @@ import { constants } from "node:http2";
  */
 export default function auth(req, res, next) {
     // next ketika berhasil
-
     const authHeader = req.headers.authorization;
+
+    if (!authHeader) {
+        res.status(constants.HTTP_STATUS_UNAUTHORIZED).json({
+            success: false,
+            message: "unauthorized",
+        });
+        return;
+    }
+
     const prefix = "Bearer ";
     const isBearer = authHeader?.startsWith(prefix);
     if (isBearer) {
         const token = authHeader.slice(prefix.length);
         const payload = VerifyToken(token);
+        // console.log("AUTH HEADER:", authHeader);
+        // console.log("TOKEN:", token);
+        // console.log("PAYLOAD:", payload);
         if (payload) {
             res.locals = payload; // dipakai untuk ambil role
             next();
             return;
         }
     }
+
     res.status(constants.HTTP_STATUS_UNAUTHORIZED).json({
         success: false,
         message: "unauthorized",
