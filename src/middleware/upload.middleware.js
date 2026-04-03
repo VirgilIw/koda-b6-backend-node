6ca19e1a69acd641977f30adbf54d6e1fa9324fc
Virgil I Ambar <imanuelambar11@gmail.com>
2026-04-03 12:09:35 +0700

    docs: add new comment in upload middleware

--- src/middleware/upload.middleware.js
+++ src/middleware/upload.middleware.js
@@ -5,12 +5,14 @@ import path from "path";
 const storage = (folder) =>
     multer.diskStorage({
         destination: function (req, file, cb) {
+            // tempat gambar disimpan di folder mana
             // err
             // path
             // nanti bisa di-upgrade pakai path.join
             cb(null, folder); 
         },
         filename: function (req, file, cb) {
+            // nama dari file didalam destination
             const randStr = nanoid();
             const ext = path.extname(file.originalname);
             const newFileName = `${randStr}${ext}`;
