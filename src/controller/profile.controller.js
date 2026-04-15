378b65b131fd5ee1e52f3cc690bca8b32c6b2d66
VirgilIw <imanuelambar11@gmail.com>
2026-04-16 03:33:15 +0700

    Refactor getProfile for improved error handling
    Refactor getProfile function to handle user data retrieval and error management more effectively.

--- src/controller/profile.controller.js
+++ src/controller/profile.controller.js
@@ -6,6 +6,35 @@ import { constants } from "node:http2";
 import fs from "fs";
 import path from "path";
 
+/**
+ *
+ * @param {import('express').Request} req - Express request object
+ * @param {import('express').Response} res - Express response object
+ * @param {import('express').NextFunction} next - Express next middleware
+ *
+ * @returns {Promise<void>}
+ */
+export async function getProfile(req, res, next) {
+    try {
+        const userId = res.locals.userId;
+
+        const data = await userModel.getUserById(userId);
+
+        if (!data) {
+            const err = new Error("data not found");
+            err.statusCode = constants.HTTP_STATUS_NOT_FOUND;
+            err.isOperational = true;
+            throw err;
+        }
+
+        return res.status(constants.HTTP_STATUS_OK).json({
+            success: true,
+            result: data,
+        });
+    } catch (error) {
+        next(error);
+    }
+}
 export async function getProfile(req, res, next) {
     try {
         const userId = res.locals.userId;
@@ -32,6 +61,8 @@ export async function getProfile(req, res, next) {
  *
  * @param {import("express").Request} req
  * @param {import("express").Response} res
+ * @param {import("express").NextFunction} next - Express next middleware
+ * @returns {void}
  */
 export function updateProfile(req, res, next) {
     const upload = uploadMiddleware("uploads/profile").single("picture");
