7a7fe16ce35472640181fbb1eaf4d104fbf2c4d6
Virgil I Ambar <imanuelambar11@gmail.com>
2026-04-16 00:08:17 +0700

    feat: add new product router in admin router

--- src/router/admin.router.js
+++ src/router/admin.router.js
@@ -2,11 +2,13 @@ import { Router } from "express";
 import userRouter from "./users.router.js";
 import auth from "../middleware/auth.middleware.js";
 import authorize from "../middleware/rbac.js";
+import productRouter from "./product.router.js";
 
 const adminRouter = Router();
 
 adminRouter.use(auth, authorize("admin"));
 
 adminRouter.use("/users", userRouter);
+adminRouter.use("/products", productRouter);
 
 export default adminRouter;
